73af7759b4091e94b42f6843e721f78c0c8af21a
Vlad Ivankov <vlad.ivankov.96@mail.ru>
2022-11-11 22:41:39 +0400

    never

--- task1.ts
+++ task1.ts
@@ -16,4 +16,53 @@
 //     title: 'Clean room',
 //     completed: false,
 // }
-type MyPick<T, K extends keyof T> = { [P in K]: T[P] };
+
+// type MyPick<T, K extends keyof T> = { [P in K]: T[P] };
+
+
+// infer - сделать вывод, вывести, опеределить
+
+/** 
+ * 
+ * 
+*/
+// type Payment = {
+//     amount: number;
+//     currency: string;
+//     currencySign?: string;
+// }
+
+// type ObjectKey  = keyof Payment;
+
+// type GetReturnType<Type> = Type extends (...args: never[]) => infer Return
+//   ? Return
+//   : never;
+  
+// type Num = GetReturnType<() => number>;
+
+// type MyType<T> = T extends Object ? 'sting' : never;
+// type T1 = MyType<{b: string}> // T1 is { b: string; }
+// type T1 = MyType<{b: string}> // T1 is { b: string; }
+
+// type Point = { x: number; y: number };
+// type P = keyof Point;
+
+// type Todo = {
+//     title: string
+//     description: string
+//     completed: boolean
+//   }
+
+
+// type test = keyof Todo
+interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+
+type Todo1 = 'description' | 'completed' | 'title'
+
+type MyOmit<T, Keys> =  T extends Keys ? Keys : never
+
+type Test = MyOmit<Todo1, 'description' | 'completed' | 'title'>
