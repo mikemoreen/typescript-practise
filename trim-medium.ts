d03f57f997b1ee39ed2eaa5f52281a292ad0ab7c
Vlad Ivankov <vlad.ivankov.96@mail.ru>
2022-11-12 22:09:08 +0400

    ...

---
+++ trim-medium.ts
@@ -0,0 +1,5 @@
+type Whitespace = " " | "\n" | "\t";
+type Trim<S extends string> = S extends ` ${infer R}` ? Trim<R> :
+                              S extends `${infer R} ` ? Trim<R> :
+                              S extends `${Whitespace}${infer R}` | `${infer R}${Whitespace}` ? Trim<R>
+                              : S
